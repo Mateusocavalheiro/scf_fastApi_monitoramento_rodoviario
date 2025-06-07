465697338e754aef42080e829efae22d21eaa98c
wateuz <wateuz1@gmail.com>
2025-06-06 22:03:38 -0300

    Atualizando Base

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://iiot_data_base_vfvv_user:110TZm9qVhnvMXWWKlTZKlWGGwsr4CPp@dpg-d0dnqgeuk2gs73db1da0-a/iiot_data_base_vfvv"
+DATABASE_URL = "postgresql://iiot_data_base_spyu_user:mUYqkhHR7rEqa3j3bp98yeC8HmoKvWpi@dpg-d11oup3uibrs73ef4v0g-a/iiot_data_base_spyu"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
